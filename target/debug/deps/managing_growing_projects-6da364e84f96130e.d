10d98e013a0ec3423f44483f3bef53021737e0e9
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-06-11 22:44:53 -0500

    packages and crates 2

---
+++ target/debug/deps/managing_growing_projects-6da364e84f96130e.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/managing_growing_projects/target/debug/deps/managing_growing_projects-6da364e84f96130e: src/main.rs
+
+/Users/admin/Documents/rust/managing_growing_projects/target/debug/deps/managing_growing_projects-6da364e84f96130e.d: src/main.rs
+
+src/main.rs:

8218a1b4e771c9db73b9f32520c6dc42f33ea963
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-06-18 22:11:13 -0500

    making_structs_enum_pub 2

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ fn main() {
             fn serve_order() {}
             
             mod back_of_house {
+                pub enum Appetizer {
+                    Soup,
+                    Salad,
+                }
+
                 pub struct Breakfast {
                     pub toast: String,
                     seasonal_fruit: String,
@@ -23,6 +28,7 @@ fn main() {
             }
                 
                 pub fn eat_at_restaurant() {
+                    
                     // Order a breakfast in the summer with Rye toast
                     let mut meal = back_of_house::Breakfast::summer("Rye");
                     // Change our mind about what bread we'd like
@@ -31,6 +37,9 @@ fn main() {
                     // The next line won't compile if we uncomment it; we're not allowed
                     // to see or modify the seasonal fruit that comes with the meal
                     // meal.seasonal_fruit = String::from("blueberries");
+
+                    let order1 = back_of_house::Appetizer::Soup;
+                    let order2 = back_of_house::Appetizer::Salad;
                 }
 
 

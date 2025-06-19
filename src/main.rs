e253de4dbe83841b3a1a16649e88c3d655010fd0
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-06-18 22:26:37 -0500

    bringing_path_into_scope_with_the_use_keyword

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,60 @@
-fn main() {
-    mod front_of_house {
-        pub mod hosting {
-            pub fn add_to_waitlist() {}
-            fn seat_at_table() {}
-        }
-        mod serving {
-            fn take_order() {}
-            fn serve_order() {}
-            
-            mod back_of_house {
-                pub enum Appetizer {
-                    Soup,
-                    Salad,
-                }
-
-                pub struct Breakfast {
-                    pub toast: String,
-                    seasonal_fruit: String,
-                }
-            impl Breakfast {
-                pub fn summer(toast: &str) -> Breakfast {
-                    Breakfast {
-                        toast: String::from(toast),
-                        seasonal_fruit: String::from("peaches"),
-                    }
-                }
-            }
-                
-                pub fn eat_at_restaurant() {
-                    
-                    // Order a breakfast in the summer with Rye toast
-                    let mut meal = back_of_house::Breakfast::summer("Rye");
-                    // Change our mind about what bread we'd like
-                    meal.toast = String::from("Wheat");
-                    println!("I'd like {} toast please", meal.toast);
-                    // The next line won't compile if we uncomment it; we're not allowed
-                    // to see or modify the seasonal fruit that comes with the meal
-                    // meal.seasonal_fruit = String::from("blueberries");
-
-                    let order1 = back_of_house::Appetizer::Soup;
-                    let order2 = back_of_house::Appetizer::Salad;
-                }
-
-
-                fn fix_incorrect_order() {
-                    cook_order();
-                    super::serve_order();
-                }
-                fn cook_order() {}
-            }
-            fn take_payment() {}
-        }
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
     }
 
-    pub fn eat_at_restaurant() {
-        // Absolute path
-        // crate::front_of_house::hosting::add_to_waitlist();
-        // Relative path
-        front_of_house::hosting::add_to_waitlist();
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+
+        fn fix_incorrect_order() {
+            cook_order();
+            serve_order();
+        }
+
+        fn cook_order() {}
     }
 }
 
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // won't compile
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+fn main() {
+    eat_at_restaurant();
+}
